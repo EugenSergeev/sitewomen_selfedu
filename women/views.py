0bf8723670f07c1c7a62de21c1845d2c58b607b8
Evgeny Sergeev <flashser@yandex.ru>
2023-10-26 13:55:02 +0300

    шаблонный тег url

--- women/views.py
+++ women/views.py
@@ -4,13 +4,17 @@ from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+menu = [
+    {'title': "О сайте", 'url_name': "about"},
+    {'title': "Добавить статью", 'url_name': "add_page"},
+    {'title': "Обратная связь", 'url_name': "contact"},
+    {'title': "Войти", 'url_name': "login"},
+]
 
 data_db = [
     {'id': 1, "title": "Анджелина Джоли", "content": "Биография Анджелины Джоли", "is_published": True},
     {'id': 2, "title": "Джессика Симпсон", "content": "Биография Джессики Симпсон", "is_published": False},
-    {'id': 3, "title": "Миа Малкова", "content": "Биография Мии Малковой", "is_published": True},
+    {'id': 34, "title": "Миа Малкова", "content": "Биография Мии Малковой", "is_published": True},
 ]
 
 
@@ -21,28 +25,26 @@ def index(request):
                }
     return render(request, 'women/index.html', context=context)
 
+
 def about(request):
     return render(request, 'women/about.html', context={'title': "About site"})
-
-def categories(request, cat_id):
-    return HttpResponse(f'<h1>Статьи по категориям</h1><p>id:{cat_id}<p>')
-
-
-def categories_by_slug(request, cat_slug):
-    if request.GET:
-        print(request.GET)
-    return HttpResponse(f'<h1>Статьи по категориям</h1><p>slug:{cat_slug}<p>')
-
-
-def archive(request, year):
-    current_year = datetime.now().year
-    if year > current_year:
-        uri = reverse('cats', args=('music',))
-        return HttpResponseRedirect(uri)
-    return HttpResponse(f'<h1>Архив по годам</h1><p>{year}<p>')
 
 
 def page_not_found(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
 
+def show_post(request, post_id):
+    return HttpResponse(f'Отображение статьи с id = {post_id}')
+
+
+def addpage(request):
+    return HttpResponse("Добавление статьи")
+
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+
+def login(request):
+    return HttpResponse(f'Авторизация')
