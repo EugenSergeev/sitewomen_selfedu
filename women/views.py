8435bac10619b1a90765475802ab4fb47d16c384
Evgeny Sergeev <flashser@yandex.ru>
2023-10-26 12:25:20 +0300

    урок 8 перенаправления redirect и reverse

--- women/views.py
+++ women/views.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 
-from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 
 def index(request):
@@ -21,7 +22,8 @@ def categories_by_slug(request, cat_slug):
 def archive(request, year):
     current_year = datetime.now().year
     if year > current_year:
-        raise Http404()
+        uri = reverse('cats', args=('music',))
+        return HttpResponseRedirect(uri)
     return HttpResponse(f'<h1>Архив по годам</h1><p>{year}<p>')
 
 
