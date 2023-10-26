be26712313cca292952a6b2fdd3a1413c8670153
Evgeny Sergeev <flashser@yandex.ru>
2023-10-26 12:42:19 +0300

    Шаблоны начало

--- women/views.py
+++ women/views.py
@@ -2,11 +2,12 @@ from datetime import datetime
 
 from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect
 from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
 from django.urls import reverse
 
 
 def index(request):
-    return HttpResponse('Страница приложения Women.')
+    return render(request, 'women/index.html')
 
 
 def categories(request, cat_id):
@@ -31,3 +32,5 @@ def page_not_found(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
 
+def about(request):
+    return render(request, 'women/about.html')
