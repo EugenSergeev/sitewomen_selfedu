196021c8e57e739d61eaf4acf6b37355edc6e67b
Evgeny Sergeev <flashser@yandex.ru>
2023-10-31 10:38:42 +0300

    Создание пользовательского менеджера

--- women/models.py
+++ women/models.py
@@ -1,9 +1,42 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
+from transliterate import slugify as transliterate_slugify
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_published=Women.Status.PUBLISHED)
 
 
 class Women(models.Model):
+
+    class Status(models.IntegerChoices):
+        DRAFT = 0, "Черновик"
+        PUBLISHED = 1, "Опубликовано"
+
     title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
+    is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
+
+    objects = models.Manager()
+    published = PublishedManager()
+
+    def __str__(self):
+        return f'<Women: {self.title}>'
+
+    def save(self, *args, **kwargs):
+        self.slug = transliterate_slugify(self.title) or slugify(self.title)
+        super().save(*args, **kwargs)
+
+    class Meta:
+        ordering = ['title']
+        indexes = [
+            models.Index(fields=['-time_create'])
+        ]
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'post_slug': self.slug})
--- women/views.py
+++ women/views.py
@@ -1,8 +1,7 @@
-from datetime import datetime
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import render, get_object_or_404
 
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
-from django.shortcuts import render
-from django.urls import reverse
+from .models import Women
 
 menu = [
     {'title': "О сайте", 'url_name': "about"},
@@ -11,51 +10,23 @@ menu = [
     {'title': "Войти", 'url_name': "login"},
 ]
 
-data_db = [
-    {'id': 1, "title": "Анджелина Джоли", "content": """<header>
-    <h2>Биография Анджелины Джоли</h2>
-</header>
-<section>
-    <h3>Ранние годы</h3>
-    <p>
-        Анджелина Джоли родилась 4 июня 1975 года в Лос-Анджелесе. Она является дочерью актёров Джона Войта и Марчелин Бертран.
-    </p>
-</section>
-<section>
-    <h3>Карьера</h3>
-    <p>
-        Анджелина начала актёрскую карьеру в раннем возрасте, но настоящую известность получила после роли в фильме "Сквозь дождь" (1998). Она получила "Оскар" за лучшую женскую роль второго плана в фильме "Прерванная жизнь" (1999).
-    </p>
-</section>
-<section>
-    <h2>Благотворительная деятельность</h2>
-    <p>
-        Кроме актёрской карьеры, Анджелина известна своей благотворительной деятельностью. Она является послом доброй воли Верховного комиссара ООН по делам беженцев.
-    </p>
-</section>
-<section>
-    <h2>Личная жизнь</h2>
-    <p>
-        Анджелина была замужем за актёрами Джонни Ли Миллером, Билли Бобом Торнтоном и Брэдом Питтом. У неё есть шесть детей, трое из которых являются приемными.
-    </p>
-</section>""", "is_published": True},
-    {'id': 2, "title": "Джессика Симпсон", "content": "Биография Джессики Симпсон", "is_published": False},
-    {'id': 34, "title": "Миа Малкова", "content": "Биография Мии Малковой", "is_published": True},
-]
-
-
 cats_db = [
     {'id': 1, 'name': "Актрисы"},
     {'id': 2, 'name': "Певицы"},
     {'id': 3, 'name': "Спортсменки"},
 ]
 
+
 def index(request):
-    context = {'title': "Главная страница",
-               'menu': menu,
-               'posts': data_db,
-               'cat_selected': 0,
-               }
+    data_db = Women.objects.filter(is_published=1)
+    data_db = Women.published.all()
+
+    context = {
+        'title': "Главная страница",
+        'menu': menu,
+        'posts': data_db,
+        'cat_selected': 0,
+    }
     return render(request, 'women/index.html', context=context)
 
 
@@ -70,8 +41,15 @@ def page_not_found(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
 
-def show_post(request, post_id):
-    return HttpResponse(f'Отображение статьи с id = {post_id}')
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
+    context = {
+        'title': post.title,
+        'menu': menu,
+        'post': post,
+        'cat_selected': 1
+    }
+    return render(request, 'women/post.html', context=context)
 
 
 def addpage(request):
@@ -87,11 +65,11 @@ def login(request):
 
 
 def show_category(request, cat_id):
-    context = {'title': "Отображение по рубрикам",
-               'menu': menu,
-               'posts': data_db,
-               'cat_selected': cat_id,
-               }
+    data_db = Women.objects.all()
+    context = {
+        'title': "Отображение по рубрикам",
+        'menu': menu,
+        'posts': data_db,
+        'cat_selected': cat_id,
+    }
     return render(request, 'women/index.html', context=context)
-
-
