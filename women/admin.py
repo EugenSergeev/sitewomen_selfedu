26e14f034f9515841b09475e3b2a57aad2d4e8d8
Evgeny Sergeev <flashser@yandex.ru>
2023-11-01 18:12:52 +0300

    Настройка админ панели

--- women/admin.py
+++ women/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from women.models import Women, Category, TagPost, Husband
+
+
+@admin.register(Women)
+class WomenAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'time_create', 'is_published', 'cat', 'husband']
+    list_display_links = ['id', 'title']
+    ordering = ['-time_create', 'title']
+    list_editable = ('is_published', 'cat', 'husband')
+    list_per_page = 5
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['name']
+
+
+@admin.register(TagPost)
+class TagPostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'tag']
+    list_display_links = ['tag']
+
+
+@admin.register(Husband)
+class HusbandAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'age']
+    list_display_links = ['name']
--- women/models.py
+++ women/models.py
@@ -14,15 +14,17 @@ class Women(models.Model):
         DRAFT = 0, "Черновик"
         PUBLISHED = 1, "Опубликовано"
 
-    title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255, unique=True, db_index=True)
-    content = models.TextField(blank=True)
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='posts')
-    tags = models.ManyToManyField('TagPost', blank=True, related_name='posts')
-    husband = models.OneToOneField('Husband', on_delete=models.SET_NULL, null=True, blank=True, related_name='woman')
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="Slug")
+    content = models.TextField(blank=True, verbose_name="Текст статьи")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
+    is_published = models.BooleanField(choices=tuple(map(lambda x: (bool(x[0]), x[1]), Status.choices)),
+                                       default=Status.DRAFT, verbose_name="Статус")
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='posts', verbose_name="Категория")
+    tags = models.ManyToManyField('TagPost', blank=True, related_name='posts', verbose_name="Теги")
+    husband = models.OneToOneField('Husband', on_delete=models.SET_NULL, null=True, blank=True, related_name='woman',
+                                   verbose_name="Муж")
 
     objects = models.Manager()
     published = PublishedManager()
@@ -32,6 +34,8 @@ class Women(models.Model):
         indexes = [
             models.Index(fields=['-time_create'])
         ]
+        verbose_name = "Известные женщины"
+        verbose_name_plural = "Известные женщины"
 
     def save(self, *args, **kwargs):
         self.slug = transliterate_slugify(self.title) or slugify(self.title)
@@ -45,8 +49,12 @@ class Women(models.Model):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
-    slug = models.CharField(max_length=255, unique=True, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name="Название")
+    slug = models.CharField(max_length=255, unique=True, db_index=True, verbose_name="Slug")
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
 
     def save(self, *args, **kwargs):
         self.slug = transliterate_slugify(self.name) or slugify(self.name)
@@ -60,8 +68,12 @@ class Category(models.Model):
 
 
 class TagPost(models.Model):
-    tag = models.CharField(max_length=100, db_index=True)
-    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+    tag = models.CharField(max_length=100, db_index=True, verbose_name="Тег")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="Slug")
+
+    class Meta:
+        verbose_name = "Тег"
+        verbose_name_plural = "Теги"
 
     def save(self, *args, **kwargs):
         self.slug = transliterate_slugify(self.tag) or slugify(self.tag)
@@ -75,9 +87,13 @@ class TagPost(models.Model):
 
 
 class Husband(models.Model):
-    name = models.CharField(max_length=255)
-    age = models.IntegerField(null=True)
-    m_count = models.IntegerField(blank=True, default=0)
+    name = models.CharField(max_length=255, verbose_name="Имя")
+    age = models.IntegerField(null=True, verbose_name="Возраст")
+    m_count = models.IntegerField(blank=True, default=0, verbose_name="Женился раз")
+
+    class Meta:
+        verbose_name = "Муж"
+        verbose_name_plural = "Мужья"
 
     def __str__(self):
         return self.name
