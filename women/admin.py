00804bcdee21e51f17a15a5cb0d17a500bdd9e06
Evgeny Sergeev <flashser@yandex.ru>
2023-11-02 10:19:07 +0300

    Настроили кастомный фильтр

--- women/admin.py
+++ women/admin.py
@@ -3,18 +3,40 @@ from django.contrib import admin, messages
 from women.models import Women, Category, TagPost, Husband
 
 
+class MarriedFilter(admin.SimpleListFilter): # кастомный фильтр
+    title = "Стутас женщин" # name of filter
+    # параметр который будет передаваться в адресной строке со значением определённым в lookups
+    parameter_name = "status"
+
+    def lookups(self, request, model_admin):
+        # возможные значения параметров
+        return [
+            ("married", "Замужем"),
+            ("single", "Не замужем")
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'married':
+            return queryset.filter(husband__isnull=False)
+        else:
+            return queryset.filter(husband__isnull=True)
+
+
 @admin.register(Women)
 class WomenAdmin(admin.ModelAdmin):
     list_display = ('title', 'time_create', 'is_published', 'cat', 'husband', 'brief_info')
-    list_display_links = ('title',)
-    ordering = ['-time_create', 'title']
-    list_editable = ('is_published', 'cat', 'husband')
-    list_per_page = 5
+    list_display_links = ('title',) # Какие столбцы являются ссылками на редактирование записи
+    ordering = ['-time_create', 'title'] # Сортировка по полям
+    list_editable = ('is_published', 'cat', 'husband') # Редактируемые поля. Нельзя редактировать поля типа ManyToMany
+    list_per_page = 5 # Пагинация
+    # Добавляет новые действия с записями. Действия должны быть прописаны в классе.
     actions = ['set_published', 'set_draft']
+    # Добавляет панель поиска. cat__name - позволяет вести поиск по конкретному полю зависимого класса
     search_fields = ('title', 'cat__name')
+    list_filter = (MarriedFilter, 'cat__name', 'is_published') # Добавляет панель фильтрации
 
     @admin.display(description="Длина статьи", ordering='content')
-    def brief_info(self, women: Women):
+    def brief_info(self, women: Women): # Добавляет новый столбец
         return len(women.content)
 
     @admin.action(description="Опубликовать")
