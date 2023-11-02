998e64c4033398b9bc1dcf4749e771d0def4f4fd
Evgeny Sergeev <flashser@yandex.ru>
2023-11-02 13:52:33 +0300

    Добавили новую статью

--- women/forms.py
+++ women/forms.py
@@ -3,9 +3,12 @@ from .models import Category, Husband
 
 
 class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255)
+    title = forms.CharField(max_length=255, label="Заголовок", widget=forms.TextInput(attrs={'class': 'form-input'}))
+    # widget=forms.TextInput(attrs={'class': 'form-input'}) - позволяет назначить кастомный класс
+
     # slug = forms.SlugField(max_length=255)
-    content = forms.CharField(widget=forms.Textarea(), required=False)
-    is_published = forms.BooleanField()
-    cat = forms.ModelChoiceField(queryset=Category.objects.all())
-    husband = forms.ModelChoiceField(queryset=Husband.objects.all(), required=False)
+    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}), required=False, label="Контент")
+    is_published = forms.BooleanField(required=False, label="Для публикации", initial=True)
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(), label="Категории", empty_label="Выбери категорию")
+    husband = forms.ModelChoiceField(queryset=Husband.objects.all(), required=False, label="Муж",
+                                     empty_label="Не замужем")
--- women/views.py
+++ women/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import AddPostForm
 from .models import Women, Category, TagPost
@@ -50,7 +50,13 @@ def addpage(request):
     if request.method == "POST":
         form = AddPostForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            try:
+                Women.objects.create(**form.cleaned_data)
+                return redirect('home')
+            except Exception as exc:
+                print(exc, exc.__class__)
+                form.add_error(None, f"Ошибка добавления поста: {exc}")
+
     else:
         form = AddPostForm()
     context = {
