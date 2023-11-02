c75cecf4198807616135971b2c45f45741cfcaa4
Evgeny Sergeev <flashser@yandex.ru>
2023-11-02 18:28:08 +0300

    Сделали форму загрузки файла

--- women/views.py
+++ women/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, get_object_or_404, redirect
 
-from .forms import AddPostForm
+from .forms import AddPostForm, UploadFileForm
 from .models import Women, Category, TagPost
 
 menu = [
@@ -24,10 +24,25 @@ def index(request):
     return render(request, 'women/index.html', context=context)
 
 
+def handle_uploaded_file(f):
+    with open(f"uploads/{f.name}", "wb") as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
+
 def about(request):
-    context = {'title': "О сайте",
-               'menu': menu,
-               }
+    if request.method == "POST":
+        form = UploadFileForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            handle_uploaded_file(form.cleaned_data['file'])
+    else:
+        form = UploadFileForm()
+    context = {
+        'title': "О сайте",
+        'menu': menu,
+        'form': form
+    }
     return render(request, 'women/about.html', context=context)
 
 
